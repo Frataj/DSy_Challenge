0bc044f6172de6a132c96c5e1409ce796b54e8d0
ccknaus <claudio.knaus@gmx.net>
2023-03-07 20:59:32 +0100

    remove Node folder

--- Node/routes/index-routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-import {indexController} from '../controller/index-controller.js';
-
-router.get("/", indexController.index.bind(indexController));
-
-export const indexRoutes = router;
